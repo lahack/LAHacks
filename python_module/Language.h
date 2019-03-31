9940d347ee4420ee7de835da1a979734373cdc3c
Lihan Zhu <lihanzhu@usc.edu>
2019-03-30 17:30:38 -0700

    add python_module

---
+++ python_module/Language.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+class Language {
+public:
+    /* Constructor. Takes path to .json */
+    Language(std::string keyPath);
+
+    /* Checks entities,
+       Returns whether all entities are found
+     */
+    bool checkEntities(std::string text, std::vector<std::string> entities);
+    
+private:
+    std::string credentialPath;
+}
